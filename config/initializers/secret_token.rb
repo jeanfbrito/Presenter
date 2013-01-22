a035591ff7a934378559cfa433992515579e33c5
vagrant <jeanfbrito@gmail.com>
2013-01-22 18:27:06 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Presenter::Application.config.secret_token = 'b2566e1cad35d86e1b8bb432b47b031ffbee0a6d91fa18e6e0e3c0b2f36e7f7ee74db591e6812e39108ddeb116581d66ffe5c3b0f6e1e3720d25bd5548120022'
