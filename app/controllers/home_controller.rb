91d1ce0c5eafd2808d1cef298853a0d990d46a8a
Eduardo Schneiders <eduardo.m.schneiders@gmail.com>
2013-01-26 20:35:42 -0200

    Trocadas aspas no select do 'about'

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-  	@about    = Page.find(:all, :conditions => 'title = "About"', :limit => 1)
+  	@about    = Page.find(:all, :conditions => "title = 'About'", :limit => 1)
   	# @location = Page.find(:all, :conditions => 'title = "Location"', :limit => 1)
   	# @speakers = Page.find(:all, :conditions => 'title = "Speakers"', :limit => 1)
   	# @schedule = Page.find(:all, :conditions => 'title = "Schedule"', :limit => 1)
