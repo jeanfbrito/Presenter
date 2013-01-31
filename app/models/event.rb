93428b0518b6bbe9f502697dc45c065efbabed23
vagrant <jeanfbrito@gmail.com>
2013-02-01 00:41:31 +0100

    resolved mass assignment problem in events speakers

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
-  attr_accessible :description, :final_date, :initial_date, :local, :title
+  attr_accessible :description, :final_date, :initial_date, :local, :title, :event_speakers_attributes
   has_many :event_speakers
   has_many :speakers, :through => :event_speakers
   accepts_nested_attributes_for :event_speakers, :allow_destroy => true
